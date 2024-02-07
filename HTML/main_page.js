a8b1be114fdd7a7e6026f37287b677635d9a84e0
yaniv99 <yaniv.bodaga99@gmail.com>
2024-02-07 21:57:44 +0200

    234e

--- HTML/main_page.js
+++ HTML/main_page.js
@@ -7,24 +7,29 @@ function verifyInput(){
     var weight = document.getElementById("weight").value;
     var alertMsg = "";
     const Mysubmit = document.getElementById("InfoButton");
+    const submitErrorMsg = document.getElementById("submitErrorMsg");
     
-	
-   
-	
-Mysubmit.onclick = function(firstName,lastName){
-    if(((checkName(firstName) == true) || firstName == '')) {
-		alertMsg = alertMsg + "Please enter a right first name.";
-	}
-	if(((checkName(lastName) == true) || lastName == '')) {
-		alertMsg = alertMsg + "Please enter a right last name.";
-    }
-    if(alertMsg != ''){
-        window.alert(alertMsg);
-}
-}
+//check if there is a digit in a given string
 function checkName(str){
         return /\d/.test(str); 
     }
 
 
+
+   
+	
+Mysubmit.onclick = function(firstName,lastName){
+    if(((checkName(firstName) == true) || firstName == '')) {
+		//alertMsg = alertMsg + "Please enter a right first name.";
+        submitErrorMsg.textContent = "Please enter a right first name.";
+	}
+	if(((checkName(lastName) == true) || lastName == '')) {
+		//alertMsg = alertMsg + "Please enter a right last name.";
+        submitErrorMsg.textContent = "Please enter a right last name."
+    }
+    if(alertMsg != ''){
+        alert(alertMsg);
 }
+}
+
+}	
