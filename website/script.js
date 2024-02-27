800a6c75bfd1e9930eddf7fa9826ead984036374
almograz1 <almograz1@gmail.com>
2024-02-27 22:31:51 +0200

    registration input validation

--- website/script.js
+++ website/script.js
@@ -39,20 +39,56 @@ function calculateBMI() {
 
 /*===============================verify input in sign up===========================*/
 function verifyInput(){
-var firstName = document.getElementById("firstName").value;
-var lastName = document.getElementById("lastName").value;
-var age = document.getElementById("age").value;
-var height = document.getElementById("height").value;
-var weight = document.getElementById("weight").value;
-var email = document.getElementById("Email").value;
-var password = document.getElementById("password").value;
-var gen = document.getElementById("gender").value;
-processInfo(email,password, firstName, lastName, age, gen, height, weight);
-alert("submitted");
-location.reload();
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var age = document.getElementById("age").value;
+  var height = document.getElementById("height").value;
+  var weight = document.getElementById("weight").value;
+  var email = document.getElementById("Email").value;
+  var password = document.getElementById("password").value;
+  var gen = document.getElementById("gender").value;
+
+  
+  if (!isValidName(firstName)) {
+    alert("Invalid First name. Please try again");
+  }
+
+  if (!isValidName(lastName)) {
+    alert("Invalid Last name. Please try again");
+  }
+
+  // Fix the age validation condition
+  if (isNaN(age) || age < 1 || age > 100) {
+    alert("Age must be between 1 and 100. Please try again");
+  }
+
+  if (isNaN(height) || height < 60 || height > 230) {
+    alert("Height should be between 60 and 230.");
+  }
+
+  if (isNaN(weight) || weight > 500 || weight < 10) {
+    alert("Weight should be between 10 and 500.");
+  }
+
+  if (!isValidEmail(email)) {
+    alert("Invalid Email address. Please try again.");
+  }
+  else
+  {
+    processInfo(email,password, firstName, lastName, age, gen, height, weight);
+    alert("submitted");
+    location.reload();
+  }
 }
 
 
+function isValidName(name) {
+  return /^[a-zA-Z]+$/.test(name) && name.trim() !== "";
+} // this function checks if name contains digits or symbols (not a-z)
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+} // this function checks if the email complies to the standard form of an email address.
 
 function showPass() {
   var x = document.getElementById("myInput");
@@ -76,3 +112,4 @@ function checkExistence(){
   }
   location.reload();
 }
+
